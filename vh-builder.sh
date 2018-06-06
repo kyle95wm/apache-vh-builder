6d957b59c4ed4bd7a6ffb0af57efa0ccd1edfec2
kyle95wm <kyle95wm@gmail.com>
2018-06-06 19:21:01 -0400

    Brought script up to ShellCheck standards

--- vh-builder.sh
+++ vh-builder.sh
@@ -4,8 +4,7 @@ if [ "$UID" != "0" ] ; then
 	exit 2
 fi
 echo "Checking if Apache is installed..."
-dpkg -L apache2 >/dev/null
-if [ $? != "0" ] ; then
+if ! dpkg -L apache2 >/dev/null ; then
 	echo "Apache2 not found! Installing now...."
 	apt-get install apache2 -y
 fi
@@ -15,14 +14,14 @@ fi
 echo "This script will help you to build a new virtual host file for Apache for any domain you enter"
 echo "But first, please answer a few questions befroe we can continue:"
 if [ -z "$domain" ] ; then
-	read -p "What is your domain name? (i.e - example.com): " domain
+	read -rp "What is your domain name? (i.e - example.com): " domain
 fi
-	echo "Generating virtual host for $domain...."
-	touch /etc/apache2/sites-available/$domain.conf
-	cat >/etc/apache2/sites-available/$domain.conf <<EOF
+	echo Generating....
+	touch /etc/apache2/sites-available/"$domain".conf
+	cat >/etc/apache2/sites-available/"$domain".conf <<EOF
 <VirtualHost *:80>
-	ServerName $domain
-	ServerAlias www.$domain
+	ServerName "$domain"
+	ServerAlias www."$domain"
 	DocumentRoot /var/www/html
 	<Directory /var/www/html/>
                 AllowOverride All
@@ -35,7 +34,6 @@ fi
 EOF
 	echo "Done!"
 	service apache2 reload
-	a2ensite $domain.conf
+	a2ensite "$domain".conf
 	service apache2 reload
 	exit
-fi
